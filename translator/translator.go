29539d6e33e8c58c085e8a835e8f7a403ea69044
Michele Finotto <m@finotto.org>
2017-10-09 12:59:10 +0200

    Initial release

---
+++ translator/translator.go
@@ -0,0 +1,25 @@
+package translator
+
+import (
+	"github.com/labstack/echo"
+	"github.com/michele/http_accept_language/language"
+)
+
+const languageKey = "github.com/michele/echo-translator/key"
+
+func SetDefault(lang string) {
+	language.Default = lang
+}
+
+func TranslatorMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		langs := language.ParseHeader(c.Request().Header.Get("Accept-Language"))
+
+		c.Set(languageKey, langs)
+		return next(c)
+	}
+}
+
+func GetLanguages(c echo.Context) language.Languages {
+	return c.Get(languageKey).(language.Languages)
+}
